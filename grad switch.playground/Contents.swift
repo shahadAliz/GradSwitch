68b1d736051ddf28a1dad57d234b2dcabff1cd08
shahadAliz <cs.shahadz@gmail.com>
2024-08-06 05:47:14 +0300

    first commit

---
+++ grad switch.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var grade = 100
+
+switch grade {
+    
+case 90...100 :
+    
+    print ("Exellent")
+    
+case 80...89 :
+    
+    print ("Very Good")
+    
+case 70...79 :
+    
+    print ("Good")
+    
+case 60...69 :
+    
+    print ("Pass")
+    
+default:
+    print ("Fail")
+}
